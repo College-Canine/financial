00beec01531f12be3ab7713bd423456e1c3e8484
William McGonagle <wpmcgonagle@gmail.com>
2024-07-12 11:56:55 -0400

    Added Logos Linking

---
+++ src/link-logos.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const banksRaw = fs.readFileSync("./data/banks.json", 'utf-8')
+const banks = JSON.parse(banksRaw)
+
+let output = {}
+
+banks.forEach(i => {
+    
+    if (i.logo != undefined) {
+
+        output[i.routing] = i.logo;        
+        
+    }
+
+})
+
+fs.writeFileSync("./data/logos.json", JSON.stringify(output))
