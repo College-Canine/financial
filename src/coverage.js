75ed61c748cd7140bf576c1a617b441b6ab2394a
William McGonagle <wpmcgonagle@gmail.com>
2024-11-21 13:01:41 -0500

    Adding More Websites

--- src/coverage.js
+++ src/coverage.js
@@ -1,45 +1,20 @@
 const fs = require("fs");
 const path = require("path");
-const google = require("buscar.io");
 const banksRaw = fs.readFileSync("../data/banks.json", "utf-8");
 const banks = JSON.parse(banksRaw);
 
-(async () => {
-  for (let i in banks) {
-    const bank = banks[i];
+let count = 0;
+let total = 0;
 
-    if (bank.website == undefined) {
-      const options = {
-        page: 0,
-        safe: false,
-        parse_ads: false,
-        additional_params: {
-          hl: "en",
-        },
-      };
+for (let i in banks) {
+  const bank = banks[i];
 
-      const response = await google.search(
-        bank.long.toLowerCase() + " " + bank.city.toLowerCase(),
-        options
-      );
-      console.log(response);
-
-      let tempSite = response.results.filter((i) => !i.is_sponsored)[0]?.url;
-
-      if (tempSite != undefined)
-        tempSite = `https://${new URL(tempSite).hostname}`;
-
-      banks[i].website = tempSite;
-      fs.writeFileSync("../data/banks.json", JSON.stringify(banks, null, 4));
-
-      return;
-    }
+  if (bank.website != undefined) {
+    count++;
   }
-})();
+  total++;
+}
 
-// let count = 0;
-// let total = 0;
-
-// console.log(
-//   count + "/" + total + " = " + ((100 * count) / total).toFixed(2) + "%"
-// );
+console.log(
+  count + "/" + total + " = " + ((100 * count) / total).toFixed(2) + "%"
+);
---
+++ src/websites.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+const google = require("buscar.io");
+const banksRaw = fs.readFileSync("../data/banks.json", "utf-8");
+const banks = JSON.parse(banksRaw);
+
+(async () => {
+  for (let i in banks) {
+    const bank = banks[i];
+
+    if (bank.website == undefined) {
+      const options = {
+        page: 0,
+        safe: false,
+        parse_ads: false,
+        additional_params: {
+          hl: "en",
+        },
+      };
+
+      const response = await google.search(
+        bank.long.toLowerCase() + " " + bank.city.toLowerCase(),
+        options
+      );
+      console.log(response);
+
+      let tempSite = response.results.filter((i) => !i.is_sponsored)[0]?.url;
+
+      if (tempSite != undefined)
+        tempSite = `https://${new URL(tempSite).hostname}`;
+
+      banks[i].website = tempSite;
+      fs.writeFileSync("../data/banks.json", JSON.stringify(banks, null, 4));
+
+      await new Promise((r) => setTimeout(r, Math.random() * 800 + 1000));
+    }
+  }
+})();
+
+// let count = 0;
+// let total = 0;
+
+// console.log(
+//   count + "/" + total + " = " + ((100 * count) / total).toFixed(2) + "%"
+// );
