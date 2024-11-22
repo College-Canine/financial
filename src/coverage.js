1c168a178f350746708e820a6ee30f04c236a7dd
William McGonagle <wpmcgonagle@gmail.com>
2024-11-22 10:22:57 -0500

    Added More Banks

--- src/coverage.js
+++ src/coverage.js
@@ -3,18 +3,38 @@ const path = require("path");
 const banksRaw = fs.readFileSync("../data/banks.json", "utf-8");
 const banks = JSON.parse(banksRaw);
 
-let count = 0;
+let website = 0;
+let logo = 0;
 let total = 0;
 
 for (let i in banks) {
   const bank = banks[i];
 
   if (bank.website != undefined) {
-    count++;
+    website++;
+  }
+  if (bank.logo != undefined) {
+    logo++;
   }
   total++;
 }
 
 console.log(
-  count + "/" + total + " = " + ((100 * count) / total).toFixed(2) + "%"
+  "Website: " +
+    website +
+    "/" +
+    total +
+    " = " +
+    ((100 * website) / total).toFixed(2) +
+    "%"
+);
+
+console.log(
+  "Logo: " +
+    logo +
+    "/" +
+    total +
+    " = " +
+    ((100 * logo) / total).toFixed(2) +
+    "%"
 );
--- src/readme.js
+++ src/readme.js
@@ -1,47 +1,78 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const banksRaw = fs.readFileSync("./data/banks.json", 'utf-8')
-const banks = JSON.parse(banksRaw)
+const banksRaw = fs.readFileSync("./data/banks.json", "utf-8");
+const banks = JSON.parse(banksRaw);
 
-const methodsRaw = fs.readFileSync("./data/methods.json", 'utf-8')
-const methods = JSON.parse(methodsRaw)
+const methodsRaw = fs.readFileSync("./data/methods.json", "utf-8");
+const methods = JSON.parse(methodsRaw);
 
 function generateMethodsTable() {
-
-    return [
-        '| Name | Logo | Colors |',
-        '| ---- | ---- | ------ |',
-        ...methods.map(i => `| ${i.name} | ![${i.name} Logo](https://raw.githubusercontent.com/College-Canine/financial/master/logos/method/${i.logo}) | ${i.colors.map(j => `![${j}](https://placehold.co/15x15/${j.slice(1)}/${j.slice(1)}.png)`).join('')} |`)
-    ].join('\n')
-
+  return [
+    "| Name | Logo | Colors |",
+    "| ---- | ---- | ------ |",
+    ...methods.map(
+      (i) =>
+        `| ${i.name} | ![${
+          i.name
+        } Logo](https://raw.githubusercontent.com/College-Canine/financial/master/logos/method/${
+          i.logo
+        }) | ${i.colors
+          .map(
+            (j) =>
+              `![${j}](https://placehold.co/15x15/${j.slice(1)}/${j.slice(
+                1
+              )}.png)`
+          )
+          .join("")} |`
+    ),
+  ].join("\n");
 }
 
 function generateBankTable() {
+  let logo = [];
 
-    let logo = []
-
-    return [
-        '| Name | Division, Country | Logo | Colors |',
-        '| ---- | ----------------- | ---- | ------ |',
-        ...banks.filter((i, n) => i.logo != undefined && !logo.includes(i.logo) && logo.push(i.logo)).map(i => `| ${i.long} | ${i.state}, ${i.country} | ![${i.long} Logo](https://raw.githubusercontent.com/College-Canine/financial/master/logos/bank/${i.logo}) | ${i.colors.map(j => `![${j}](https://placehold.co/15x15/${j.slice(1)}/${j.slice(1)}.png)`).join('')} |`)
-    ].join('\n')
-
+  return [
+    "| Name | Division, Country | Logo | Colors |",
+    "| ---- | ----------------- | ---- | ------ |",
+    ...banks
+      .filter(
+        (i, n) =>
+          i.logo != undefined && !logo.includes(i.logo) && logo.push(i.logo)
+      )
+      .filter((i, n) => n < 25)
+      .map(
+        (i) =>
+          `| ${i.long} | ${i.state}, ${i.country} | ![${
+            i.long
+          } Logo](https://raw.githubusercontent.com/College-Canine/financial/master/logos/bank/${
+            i.logo
+          }) | ${i.colors
+            .filter((i, n) => n < 4)
+            .map(
+              (j) =>
+                `![${j}](https://placehold.co/15x15/${j.slice(1)}/${j.slice(
+                  1
+                )}.png)`
+            )
+            .join("")} |`
+      ),
+  ].join("\n");
 }
 
 function generateReadme() {
-
-    return [
-        `# Financials`,
-        `💰 Logos and metadata for thousands of financial institutions.`,
-        `## Banks`,
-        generateBankTable(),
-        `## Methods`,
-        generateMethodsTable(),
-        `## Legal Notice`,
-        `College Canine does not claim to own any copyrights or trademarks represented in this repository besides the College Canine Logo and Name. All licensing of logos and trademarks should be done with their respective owners.`
-    ].join('\n\n') + '\n'
-
+  return (
+    [
+      `# Financials`,
+      `💰 Logos and metadata for thousands of financial institutions.`,
+      `## Banks`,
+      generateBankTable(),
+      `## Methods`,
+      generateMethodsTable(),
+      `## Legal Notice`,
+      `College Canine does not claim to own any copyrights or trademarks represented in this repository besides the College Canine Logo and Name. All licensing of logos and trademarks should be done with their respective owners.`,
+    ].join("\n\n") + "\n"
+  );
 }
 
-fs.writeFileSync('./readme.md', generateReadme())
+fs.writeFileSync("./readme.md", generateReadme());
